5e9cd2d60e91a407a8667f77edfe8256486e8a54
mc air <mcair@Temwanis-MacBook-Air.local>
2024-05-10 10:38:11 +0200

    Now with header batches for more speed

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.50 Safari/537.36'
 
 #proxies = { 'http': 'http://localhost:8080', 'https': 'http://localhost:8080' }
 proxies = { }
@@ -21,7 +22,12 @@ def main():
     parser.add_argument('-u', '--url', help='URL to send requests to')
     parser.add_argument('-f', '--file', help='File containing URLs to send requests to')
     parser.add_argument('-c', '--canary', required=True, help='Canary value')
+    parser.add_argument('-n', '--num-headers', type=int, default=1, help='Number of headers to add per request')
+    
+    counter = 1
     args = parser.parse_args()
+
+    split = args.num_headers
 
     if args.url:
         urls = [args.url]
@@ -34,7 +40,7 @@ def main():
 
     for url in urls:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.50 Safari/537.36'
+            'User-Agent': user_agent
         }
 
         print(colored(f'[+] Sending requests to {url}', 'green'))
@@ -62,26 +68,65 @@ def main():
                 print(colored(f'[+] Adding header: {header}', 'yellow'))  # Print the name of the header in yellow
 
         total_requests = len(headers_list)
-        for i, header in enumerate(headers_list, start=1):
-            headers[header] = args.canary
+
+        if split > total_requests:
+            split = total_requests
+
+        batches_of_headers = []
+
+        for i in range(0, total_requests, split):
+            batches_of_headers.append(headers_list[i:i+split])
+
+        print(colored(f'[+] Headers per request: {split}', 'yellow'))
+        
+        total_batches = len(batches_of_headers)
+
+        for header_batch in batches_of_headers:
+            print(f'Request Batch: {counter}/{total_batches}', end='\r')
+            counter += 1
+
+            headers = {
+                'User-Agent': user_agent
+            }
+
+            for header in header_batch:
+                headers[header] = args.canary
+
             params = {random_string(): args.canary}
-            response = requests.get(url, headers=headers, params=params, verify=False, proxies=proxies
-                                    )
+            response = requests.get(url, headers=headers, params=params, verify=False, proxies=proxies)
             response_size = len(response.text)
             response_status = response.status_code
 
-            print(f'Request number: {i}, Remaining requests: {total_requests - i}\r', end='', flush=True)
+            #Did batch trigger
+            if args.canary in response.text or response_status != base_response_status or response_size != base_response_size:
 
-            if args.canary in response.text:
-                print(colored(f'[+] Canary {args.canary} found in response for header: {header}', 'green'))
 
-            elif response_status != base_response_status:
-                print(colored(f'[+] Response status changed to {response_status} for header: {header}', 'green'))
 
-            elif response_size != base_response_size:
-                print(colored(f'[+] Response size changed from {base_response_size} to {response_size} for header: {header}', 'green'))
+                for header in header_batch:
+                    headers = {
+                        'User-Agent': user_agent
+                    }
 
-            del headers[header]  # Remove the custom header after the request is sent
+                    headers[header] = args.canary
+                    params = {random_string(): args.canary}
+                    response = requests.get(url, headers=headers, params=params, verify=False, proxies=proxies)
+                    response_size = len(response.text)
+                    response_status = response.status_code
+
+                    
+
+                    if args.canary in response.text:
+                        print(colored(f'[+] Canary {args.canary} found in response for header: {header}', 'green'))
+
+                    elif response_status != base_response_status:
+                        print(colored(f'[+] Response status changed to {response_status} for header: {header}', 'green'))
+
+                    elif response_size != base_response_size:
+                        print(colored(f'[+] Response size changed from {base_response_size} to {response_size} for header: {header}', 'green'))
+
+                    del headers[header]
+
+
 
 if __name__ == "__main__":
     main()
